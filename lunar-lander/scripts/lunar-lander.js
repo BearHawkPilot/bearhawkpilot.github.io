5b0ac6bfbe93b96bd5f9fb1632fd0f3f4c0840a1
BearHawkPilot <bearhawkpilot@gmail.com>
2022-11-28 21:53:15 -0700

    Add code and files to bring project up-to-date

---
+++ lunar-lander/scripts/lunar-lander.js
@@ -0,0 +1,88 @@
+const gameCanvas = document.getElementById('gameCanvas');
+gameCanvas.clientWidth = innerWidth / 3; // TODO: should we be using innerWidth/innerHeight or some other width and height?
+gameCanvas.clientHeight = innerHeight / 5;
+const ctx = gameCanvas.getContext('2d');
+
+let clickCounter = 0;
+gameCanvas.addEventListener('click', onclick);
+
+let haveFullScreen = false;
+
+drawIntro();
+
+///////////////
+// Functions //
+///////////////
+
+function drawIntro() {
+    drawBackground();
+    drawInstructions();
+    drawDebugText();
+}
+
+function drawBackground() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, gameCanvas.clientWidth, gameCanvas.clientHeight);
+}
+
+function drawInstructions() {
+  ctx.fillStyle = 'white';
+  ctx.textAlign = 'center'
+  const againText = clickCounter > 0 ? 'again ' : "";
+
+  if (gameCanvas.clientHeight < screen.height) {
+      ctx.fillText('Click ' + againText + 'to try to get full screen', gameCanvas.clientWidth / 2, gameCanvas.clientHeight / 2);
+  }
+  else {
+      haveFullScreen = true;
+      ctx.fillText('100% full screen achieved.  Click to play', gameCanvas.clientWidth / 2, gameCanvas.clientHeight / 2)
+  }
+}
+
+function drawDebugText() {
+  ctx.fillText(
+      `${clickCounter} clicks.  Resolution: ${gameCanvas.clientWidth}/${screen.width} x ${gameCanvas.clientHeight}/${screen.height}`,
+      gameCanvas.clientWidth / 2,
+      (gameCanvas.clientHeight / 2) + 12
+  );
+}
+
+function startGame() {
+  draw();
+}
+
+function draw() {
+  drawBackground();
+  drawTerrain();
+  drawLander();
+}
+
+function drawTerrain() {
+}
+
+function drawLander() {
+  const landerImage = new Image();
+  landerImage.src = 'images/lander.png';
+  landerImage.addEventListener('load', (e) => {
+      ctx.drawImage(landerImage, 50, 50);
+  })
+
+}
+
+////////////////////
+// Event handlers //
+////////////////////
+
+function onclick() {
+  ++clickCounter;
+
+  if (!haveFullScreen) {
+      gameCanvas.requestFullscreen();
+      gameCanvas.width = innerWidth;
+      gameCanvas.height = innerHeight;
+      drawIntro();
+  } else {
+      gameCanvas.removeEventListener('click', onclick)
+      startGame();
+  }
+}
